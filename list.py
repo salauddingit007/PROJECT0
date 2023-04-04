5dbe8c37657ba84b7b21632162f94acf16fcd121
salauddingit007 <salauddingit007@gmail.com>
2023-04-04 07:08:43 +0600

    list added

---
+++ list.py
@@ -0,0 +1,32 @@
+# List items are ordered, changeable, and allow duplicate values.
+# List items are indexed, the first item has index [0], the second item has index [1] etc.pro
+programminglanguage =[ "c","c++","js","python"]
+
+print("python" in programminglanguage)
+
+# There are four collection data types in the Python programming language:
+
+# List is a collection which is ordered and changeable. Allows duplicate members.
+# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+# Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+# Dictionary is a collection which is ordered** and changeable. No duplicate members.
+
+programminglanguage =[ "c","c++","js","python"]
+
+print("python " * 3)
+
+programminglanguage =[ "elex","c", "c++","js","python"]
+
+programminglanguage.insert(2,"os")
+
+programminglanguage.remove("js")
+
+programminglanguage.sort()
+
+programminglanguage.append("c")
+
+pos=programminglanguage.index("c")
+
+print(len(programminglanguage))
+
+print(programminglanguage)
