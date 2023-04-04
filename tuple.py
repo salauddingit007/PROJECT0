12ba89cb88a0171a652673a97307ce3edbd7a168
salauddingit007 <salauddingit007@gmail.com>
2023-04-04 10:50:39 +0600

    set added

---
+++ tuple.py
@@ -0,0 +1,12 @@
+# Tuple items are ordered, unchangeable, and allow duplicate values.
+# Tuple items are indexed, the first item has index [0], the second item has index [1] etc.
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-2])
+
+students = (
+    ("jahid",18192103119),
+    "sajib",
+    "jannat",
+)
+# students[0]="elias"
+print(students [0])
